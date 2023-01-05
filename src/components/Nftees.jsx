bd4db1343655e3a93ca8df8367bbd760b67b90f1
unknown <jenheidt1@gmail.com>
2023-01-05 17:11:14 -0500

    added communiTees collection to my tees

--- src/components/Nftees.jsx
+++ src/components/Nftees.jsx
@@ -1,21 +1,197 @@
-import "../App.css";
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import "./NftviewApp.css";
 
-export default function Nftees() {
-  return (
-    <div className="min-content mynftees">
+function App() {
+  const [haveMetamask, sethaveMetamask] = useState(true);
+  const [accountAddress, setAccountAddress] = useState('');
+  const [accountBalance, setAccountBalance] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+  const [balance, setBalance] = useState('');
+  const [ownedNFTs, setOwnedNFTs] = useState([]);
+  const [imageURLs, setImageURLs] = useState([]);
+  const [names, setNames] = useState([]);
+
+
+  const { ethereum } = window;
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const contractAddress = '0x633763D9174d6B772676920b2309b39eE3A92a8a';
+  const contractABI = require('../components/contract ABIs/CommuniTeesABI.json');
+  
+  const contract = new ethers.Contract(contractAddress, contractABI, provider);
+  
+  
+  useEffect(() => {
+    const { ethereum } = window;
+    const checkMetamaskAvailability = async () => {
+      if (!ethereum) {
+        sethaveMetamask(false);
+      }
+      sethaveMetamask(true);
+    };
+    checkMetamaskAvailability();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        sethaveMetamask(false);
+      }
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      let balance = await provider.getBalance(accounts[0]);
+      let bal = ethers.utils.formatEther(balance);
+      setAccountAddress(accounts[0]);
+      setAccountBalance(bal);
+      setIsConnected(true);
+    } catch (error) {
+      setIsConnected(false);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchBalance() {
+      const balance = await contract.balanceOf(accountAddress);
+      console.log('balance:', balance);
+      setBalance(balance);
+    }
+    if (accountAddress) {
+      fetchBalance();
+    }
+  }, [accountAddress]);
+
+  useEffect(() => {
+   async function fetchOwnedNFTs() {
+  try {
+    const ownedNFTs = [];
+    let index = 0;
+    let tokenId;
+    let balanceOf;
+    let counter = 0;
+    
+    // Retrieve the balance of the contract
+    balanceOf = await contract.balanceOf(accountAddress);
+    
+    // Use the balance of the contract as the maximum number of iterations
+    const maxIterations = balanceOf.toNumber(); 
+    
+    while (counter < maxIterations) {
+      try {
+        tokenId = await contract.tokenOfOwnerByIndex(accountAddress, index);
+        ownedNFTs.push(tokenId);
+        index++;
+      } catch (error) {
+        console.error(error);
+        break;
+      }
+      counter++;
+    }
+    setOwnedNFTs(ownedNFTs.map(ownedNFT => ownedNFT.toString()));
+    const { imageURLs, names } = await getImageURLs();
+    setImageURLs(imageURLs);
+    setNames(names);
+  } catch (error) {
+    // Handle any other errors
+  }
+}
+
+    if (accountAddress) {
+      fetchOwnedNFTs();
+    }
+  }, [accountAddress, contract]);
+  
+  const getImageURLs = async () => {
+    const imageURLs = [];
+    const names = [];
+    for (const tokenId of ownedNFTs) {
+      const tokenURI = await contract.tokenURI(tokenId);
+      const response = await fetch(tokenURI);
+      const tokenData = await response.json();
+      imageURLs.push(tokenData.image);
+      names.push(tokenData.name);
+    }
+    return { imageURLs, names };
+  }
+
+  function NFTName(props) {
+    return <p>{props.name}</p>
+  }
+  
+  function NFTImage(props) {
+    return (
       <div>
-      <a href="./App">
-      <button className="closebutton">x Close</button>
-      </a>
+        <img src={props.imageURL} alt="NFT" />
+        <p>{props.name}</p>
       </div>
-      <div className="border-1">
-        <div className="border-1">
-          <div className="border-1">
-            <p className="open-closet">Open Wardrobe</p>
+    );
+  }
+  
+  
+  
+ 
+  return (
+    <div className="App">
+      {haveMetamask ? (
+        <>
+          <div className="top-right-container">
+            <a href="./App" className="close-button">
+              Close
+            </a>
           </div>
-          <div className="textcenter"><h3>🚧Temporarily down for maintenance🚧</h3></div>
-        </div>
-      </div>
+          <div className="left-container">
+            {isConnected ? (
+              <p className="connected-message">Connected Successfully</p>
+            ) : (
+              <button className="connect-button" onClick={connectWallet}>
+                Open Fantom Wardrobe
+              </button>
+            )}
+            <div className="wallet-card">
+              {isConnected ? (
+                <>
+                  <div className="card-row">
+                    <h3>Wallet Address:</h3>
+                    <p>
+                      {accountAddress.slice(0, 4)}...
+                      {accountAddress.slice(38, 42)}
+                    </p>
+                  </div>
+                  <div className="card-row">
+                    <h3>Wallet Balance:</h3>
+                    <p>$FTM {accountBalance}</p>
+                  </div>
+                </>
+              ) : null}
+            </div>
+          </div>
+          {isConnected ? (
+            <div className="nft-container">
+              {ownedNFTs.length > 0 ? (
+                <>
+                  <h1 className="title">CommuniTees Collection</h1>
+                  <div className="nft-cards">
+                    {ownedNFTs.map((nft, i) => (
+                      <div className="nft-card" key={nft}>
+                        <NFTImage imageURL={imageURLs[i]} />
+                        <h4>{names[i]}</h4>
+                      </div>
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <p>No NFTs owned</p>
+              )}
+            </div>
+          ) : null}
+        </>
+      ) : (
+        <p>Please Install MetaMask</p>
+      )}
     </div>
   );
+  
+
 }
+
+    export default App;
--- src/components/wallettest.jsx
+++ src/components/wallettest.jsx
@@ -1,28 +1,117 @@
-import "../App.css";
-import CommunityABI from "../components/contract ABIs/CommuniTeesABI.json";
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import contractABI from '../components/contractABIs/CommuniTeesABI.json';
 
-const ethers = require("ethers");
-const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/fantom');
-const CommunityABI = {CommunityABI}
-const CommunityAddress = '0x633763D9174d6B772676920b2309b39eE3A92a8a'
-const CommunityContract = new ethers.Contract(CommunityAddress, CommunityABI, provider)
+function App() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [walletAddress, setWalletAddress] = useState('');
+  const [chainId, setChainId] = useState('');
+  const [contract, setContract] = useState(null);
 
-const main = async () => {
-    const Communityname = await CommunityContract.name();
-    console.log(Communityname);
+  async function connectWallet() {
+    try {
+      // Check if the user has already connected their wallet
+      if (!window.ethereum.selectedAddress) {
+        // Request access to MetaMask
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-      
-      <div className="min-content ultra-exclusive">
-         <div>
-        <a href="./App">
-        <button className="closebutton">x Close</button>
-        </a>
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
+  useEffect(() => {
+    async function checkNetwork() {
+      try {
+        // Check the user's current network
+        const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+        // Convert the chain ID to a number
+        const currentChainId = parseInt(chainId.result, 16);
+
+        setChainId(currentChainId);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    checkNetwork();
+  }, []);
+
+  useEffect(() => {
+    async function checkWallet() {
+      try {
+        // Check if the user has connected a wallet
+        if (window.ethereum.selectedAddress) {
+          setWalletConnected(true);
+          setWalletAddress(window.ethereum.selectedAddress);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    checkWallet();
+  }, []);
+
+  useEffect(() => {
+    async function checkAccount() {
+      try {
+        // Check the user's current account
+        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        if (accounts.length > 0) {
+          setWalletConnected(true);
+          setWalletAddress(accounts[0]);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    checkAccount();
+  }, []);
+
+  useEffect(() => {
+    async function connectToContract() {
+      try {
+        // Connect to the contract using the provided ABI and address
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          '0x633763D9174d6B772676920b2309b39eE3A92a8a',
+          contractABI,
+          signer
+        );
+
+        setContract(contract);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    connectToContract();
+  }, []);
+
+  return (
+    <div>
+      {walletConnected ? (
+        <p>
+          Wallet connected! Network: {chainId} Wallet address: {walletAddress}
+        </p>
+      ) : (
+        <button onClick={connectWallet}>Connect wallet</button>
+      )}
+      {contract ? (
+        <p>Connected to contract!</p>
+      ) : (
+        <button onClick={connectToContract}>Connect to contract</button>
+        )}
         </div>
-        <div className="box">
-        <p className="box-title">{main}</p>
-          <p className="box-details">
-         Test
-          </p>
-        </div>
-      </div>
-}
+      );
+    }
+    
+    export default App;
+    
